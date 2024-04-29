2437ad532a197f2fc6299809fc35533c3e35dc93
Oleksandr <oleksandr.ovcharenko@dci-students.org>
2024-04-29 14:07:01 +0200

    Dictionary updated

--- src/main/java/dci/j24e01/wordle/wordle/Game.java
+++ src/main/java/dci/j24e01/wordle/wordle/Game.java
@@ -5,18 +5,23 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Optional;
 
 public class Game {
     ArrayList<String> words;
     String secret;
+    char[] secretArray;
 
     public Game() {
         words = new ArrayList<>();
         //secret = Dictionary.getRandomWord();
         secret = "ready";
+        secretArray = secret.toCharArray();
     }
 
     public boolean guessed(String word) {
@@ -39,20 +44,21 @@ public class Game {
     }
 
     public void setLabelsContent(String word, Label[] labels) {
+        char[] chars = word.toCharArray();
+        isCorrectPosition(chars);
+        contains(chars);
 
         for (int i = 0; i < 5; i++) {
             labels[i].setText(word.charAt(i) + "");
             labels[i].setStyle("");
             labels[i].setAlignment(Pos.CENTER);
 
-            if (secret.charAt(i) == word.charAt(i)) {
+            if (chars[i] == ' ') {
                 labels[i].setStyle("-fx-background-color: rgb(0, 255, 0); -fx-font-size:24;");
-
-            } else if (secret.contains(word.charAt(i) + "")) {
+            } else if (chars[i] == '.') {
                 labels[i].setStyle("-fx-background-color: rgb(255, 200, 0); -fx-font-size:24;");
-
             } else {
-                labels[i].setStyle("-fx-background-color: rgb(219, 219, 217); -fx-font-size:24;");
+                labels[i].setStyle("-fx-background-color: rgb(175, 175, 175); -fx-font-size:24;");
             }
 
         }
@@ -65,9 +71,19 @@ public class Game {
         ButtonType customNoButton = new ButtonType("Exit", ButtonBar.ButtonData.NO);
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "", customYesButton, customNoButton);
         if (won) {
-            alert.setContentText("Congratulations! You won!!!");
+            alert.setTitle("You're a winner! Awesome!");
+            alert.setHeaderText("Congratulations! You won!!!");
+            ImageView icon = new ImageView("file:src/main/java/dci/j24e01/wordle/wordle/resources/winner.png");
+            icon.setFitHeight(70);
+            icon.setFitWidth(70);
+            alert.setGraphic(icon);
         } else {
-            alert.setContentText("Whooops... You Lose =( The secret word was " + secret + ".");
+            alert.setTitle("Better luck next time!");
+            alert.setHeaderText("Whooops... You Lose :( \nThe secret word was \"" + secret + "\".");
+            ImageView icon = new ImageView("file:src/main/java/dci/j24e01/wordle/wordle/resources/broken_cup.png");
+            icon.setFitHeight(70);
+            icon.setFitWidth(70);
+            alert.setGraphic(icon);
         }
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent()) {
@@ -75,6 +91,27 @@ public class Game {
                 System.exit(0);
             }
         }
+    }
 
+    private char[] isCorrectPosition (char[] chars) {
+        for (int i = 0; i < 5; i++) {
+            if (chars[i] == secretArray[i]) {
+                chars[i] = ' ';
+            }
+        }
+        return chars;
+    }
+
+    private char[] contains(char[] chars) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (chars[j] == secretArray[i]) {
+                    chars[j] = '.';
+                    break;
+                }
+            }
+        }
+        return chars;
     }
 }
--- src/main/java/dci/j24e01/wordle/wordle/Wordle.java
+++ src/main/java/dci/j24e01/wordle/wordle/Wordle.java
@@ -3,6 +3,7 @@ package dci.j24e01.wordle.wordle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,11 @@ import java.io.IOException;
 public class Wordle extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Wordle.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Wordle.class.getResource("wordle.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 350, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Wordle");
+        System.out.println(Wordle.class.getResourceAsStream("/logo.png"));
+        //stage.getIcons().add(new Image(Wordle.class.getResourceAsStream("/logo.png")));
         stage.setScene(scene);
         stage.show();
     }
